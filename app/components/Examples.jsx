844121a66969938365df87ae412ee9fc42d9eadc
Vidura <Vidura@Viduras-MacBook-Air.local>
2017-03-31 17:45:45 +0800

    add grid and examples links

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 
 var Examples = (props)=>{
   return(
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to="/?location=Colombo">Colombo, Sri Lanka</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
 
