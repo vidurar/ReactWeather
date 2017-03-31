d034de193b3758caf3f808d45ef282425bd92ee3
Vidura <Vidura@Viduras-MacBook-Air.local>
2017-03-31 17:51:10 +0800

    About page styling done

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require("react");
 
 var About = (props)=>{
   return(
-    <h3>About</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>A simple weather application built on React</p>
+      <p>Here are some of the tools I used</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a>
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map API</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
